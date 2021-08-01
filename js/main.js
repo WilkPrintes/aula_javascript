7c94fcbf9edfe7ab96d2f3c8fe1a7134cd15fc4e
WilkPrintes <wilkp90@gmail.com>
2021-08-01 20:44:31 -0300

    modified main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,54 @@
-let nome = "Wilk Printes";
-let idade = 18;
-let idade2 = 10;
-let frase = "Japão é o melhor time do mundo";
-// alert(nome + " tem " + idade + " anos");
+//Variais e Arreys
 
-console.log(`${nome} tem ${idade + idade2} anos`);
-console.log(frase.replace("Japão", "Brasil").toUpperCase());
-console.error("Deu ruim irmão");
+// let nome = "Wilk Printes";
+// let idade = 18;
+// let idade2 = 10;
+// let frase = "Japão é o melhor time do mundo";
+// // alert(nome + " tem " + idade + " anos");
+
+// console.log(`${nome} tem ${idade + idade2} anos`);
+// console.log(frase.replace("Japão", "Brasil").toUpperCase());
+// console.error("Deu ruim irmão");
+
+// let lista = ["maçã", "pêra", "laranja"];
+// lista.push("Uva");
+// // lista.pop();
+// // let lista2 = lista.reverse();
+// // console.log(lista2);
+// // console.log(lista2.toString());
+// // console.log(lista2.join(" | "));
+
+// let fruta = {nome:"maça", cor:"vermelho"};
+// console.log(fruta.cor);
+
+// let frutas = [{nome:"maça", cor:"vermelho"}, {nome:"uva", cor:"roxo"}];
+// console.log(frutas[0].nome + " " + frutas[0].cor + " | " + frutas[1].nome + " " + frutas[1].cor) ;
+
+//Conficionais
+
+// let idade = prompt("Qual sua idade?");
+// if (idade >= 18){
+//     console.log("Maior de Idade");
+// }else{
+//     console.log("Menor de Idade");
+// }
+
+//Estrutura de Repetição
+
+// let count = 0;
+
+// while(count <= 5)
+// {
+//     console.log(count);
+//     count++;
+// }
+
+// for (let count = 0; count <= 5; count++)
+// {
+//     console.log(count);
+// }
+
+// Data
+// let date = new Date();
+
+// console.log(date.getHours());
