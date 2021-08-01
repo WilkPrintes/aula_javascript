0987cd144d1240b8cc2cfa53763ed0605f409179
WilkPrintes <wilkp90@gmail.com>
2021-08-01 18:49:01 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nome = "Wilk Printes";
+let idade = 18;
+let idade2 = 10;
+let frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + idade + " anos");
+
+console.log(`${nome} tem ${idade + idade2} anos`);
+console.log(frase.replace("Japão", "Brasil").toUpperCase());
+console.error("Deu ruim irmão");
